8dd1c7dd75d562948b6bae3ac6f915ef9f55d0d4
Ahmad mujahed altamimi <arabgamer92@gmail.com>
2026-04-01 01:53:10 +0300

    homepage design start

---
+++ js/chart.js
@@ -0,0 +1,25 @@
+
+const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0.4,
+      backgroundColor: "#D4AF37",
+      borderColor: "#D4AF37",
+      data: yValues,
+      pointRadius: 0,
+      pointHoverRadius: 0,
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 6, max:16}}],
+    }
+  }
+});
