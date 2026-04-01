9e355aedd781521a61ea17f6e90bd35b3d233988
dana-khabbas <159653208+dana-khabbas@users.noreply.github.com>
2026-04-01 16:55:48 +0300

    dana wednesday work

---
+++ js/myAssetForm.js
@@ -0,0 +1,7 @@
+let assetName=document.getElementById("assetName");
+let assetType=document.getElementById("assetType");
+let assetKirat=document.getElementById("kirat");
+let assetWeight=document.getElementById("weight");
+let assetPurchasePrice=document.getElementById("purchasePrice");
+let assetPurchaseDate=document.getElementById("purchaseDate");
+let assetImage=document.getElementById("assetImage")
