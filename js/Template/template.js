50e1dd52412b5f072d1f9c666ac48f6cbf018c7d
AR92-Dev <arabgamer92@gmail.com>
2026-04-02 15:18:30 +0300

    change currenct and chart

--- js/Template/template.js
+++ js/Template/template.js
@@ -1,4 +1,4 @@
-fetch('./Template/header.html')
+fetch('./html/Template/header.html')
     .then(response => {
         if (!response.ok) throw new Error("Header not found!");
         return response.text();
@@ -8,7 +8,7 @@ fetch('./Template/header.html')
     })
     .catch(error => console.error(error));
 
-fetch('./Template/footer.html')
+fetch('./html/Template/footer.html')
     .then(response => {
         if (!response.ok) throw new Error("Footer not found!");
         return response.text();
@@ -17,3 +17,11 @@ fetch('./Template/footer.html')
         document.getElementById('footer-placeholder').innerHTML = data;
     })
     .catch(error => console.error(error));
+    const login = document.getElementById("login")
+    if(sessionStorage.getItem("isLoggedIn") !== "true"){
+            login.style.visibility=false
+        }
+        else{
+            login.style.visibility=false
+
+        }
