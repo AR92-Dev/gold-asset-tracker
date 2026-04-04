da1a61124b42a959887e8240c57671b008d3a9e8
Ahmad mujahed altamimi <arabgamer92@gmail.com>
2026-04-04 16:38:03 +0300

    logout

--- js/Template/template.js
+++ js/Template/template.js
@@ -1,22 +1,36 @@
-fetch('/html/Template/header.html') //path
+fetchdata();
+
+function fetchdata() {
+    fetch('/html/Template/header.html')
     .then(response => {
         if (!response.ok) throw new Error("Header not found!");
         return response.text();
     })
     .then(data => {
+
         document.getElementById('header-placeholder').innerHTML = data;
+        
         const showDiv = document.getElementById("show");
-        if (showDiv) {
+        const logout = document.getElementById("logout"); 
+        
+        if (showDiv && logout) {
             if (localStorage.getItem("isLoggedIn") === "true") {
                 showDiv.style.display = "none"; 
+                logout.style.display = "block";
             } else {
                 showDiv.style.display = "flex"; 
+                logout.style.display = "none";
             }
+            logout.addEventListener("click", () => {
+                localStorage.setItem("isLoggedIn", "false");
+                localStorage.setItem("currentUser", "");
+                fetchdata(); 
+            });
         }
     })
     .catch(error => console.error(error));
 
-fetch('/html/Template/footer.html')
+    fetch('/html/Template/footer.html')
     .then(response => {
         if (!response.ok) throw new Error("Footer not found!");
         return response.text();
@@ -25,12 +39,4 @@ fetch('/html/Template/footer.html')
         document.getElementById('footer-placeholder').innerHTML = data;
     })
     .catch(error => console.error(error));
-
-    // const login = document.getElementById("login")
-    // if(sessionStorage.getItem("isLoggedIn") !== "true"){
-    //         login.style.visibility=false
-    //     }
-    //     else{
-    //         login.style.visibility=false
-
-    //     }
+}
